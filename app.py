8a3acc2d7f3fe374310ea231b8a2fc539d977dc2
Legends Legecy <163406769+LegendsLegecy@users.noreply.github.com>
2025-10-20 01:29:20 -0700

    initial

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def birthday_surprise():
+    return render_template('index.html')
+
+@app.route('/birthday')
+def birthday_surprise_page():
+    return render_template('birthday_surprise.html')
+
+# For Vercel deployment
+def handler(request):
+    return app(request)
